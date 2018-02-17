e668202aa2f2d2b997673d73d10553ebfc8ce925
ryz310 <ryz310@gmail.com>
2018-02-17 23:54:41 +0900

    Implements #greet method

--- lib/test_gem.rb
+++ lib/test_gem.rb
@@ -1,5 +1,7 @@
 require "test_gem/version"
 
 module TestGem
-  # Your code goes here...
+  def self.greet
+    'Hello World!'
+  end
 end
--- spec/test_gem_spec.rb
+++ spec/test_gem_spec.rb
@@ -3,7 +3,9 @@ RSpec.describe TestGem do
     expect(TestGem::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#greet' do
+    it 'returns "Hello World!"' do
+      expect(TestGem.greet).to eq('Hello World!')
+    end
   end
 end
